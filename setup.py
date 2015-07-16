c5d15ae794ac689df80ee14be0c95eaf3e2e43bf
biryani <abhijith31792@gmail.com>
2015-07-16 13:20:52 +0530

    Adding setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='Quacpy',
+      version='0.0',
+      description='Quantum computer simulator using Numpy',
+      author='Abhijith J /biryani',
+      url='https://github.com/biryani/Quacpy',
+      packages=['auxfun','qregister','qoperator'],
+     )
